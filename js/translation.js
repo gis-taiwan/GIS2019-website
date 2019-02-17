4e7e3b0c1eef2f8ada347fdae121c66f814b70f1
james60708 <b05505004@ntu.edu.tw>
2019-02-17 17:15:59 +0800

    transplete complete

--- js/translation.js
+++ js/translation.js
@@ -1,13 +1,42 @@
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 var lang = ['en','zh'];
 var current = 0;
+var userlang = getCookie('language'); 
+if(userlang.match('en')){
+    current = 0;
+}
+else if(userlang.match('zh')){
+    current = 1;
+}
+else{ // cookie === "undefined"
+    //console.log(userlang);
+    current = 0;
+    document.cookie = "language='en'; path=/";
+}
+
 i18next
   .use(i18nextXHRBackend)
   .init({
-      //load: ['en','zh-tw'],
+      lng : lang[current],
+      load: ['en','zh'],
       fallbackLng: 'en',
-      debug: true,
+      debug: false,
       backend: {
-          loadPath: './locales/{{lng}}.json'
+          loadPath: './locales/{{lng}}.json' 
       }
 
   }, function(err, t) {
@@ -17,6 +46,8 @@ i18next
 
       $('.lang-toggle').click(function() {
           current ^= 1;
+          document.cookie = "language='" + lang[current] + "'; path=/";
+          //console.log(document.cookie);
           i18next.changeLanguage(lang[current], function() {
               $('.whole').localize();
           });
